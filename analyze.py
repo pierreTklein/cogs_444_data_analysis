b16ba55bc2efd28f73021418693f073bc48d422b
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2019-04-02 15:47:13 -0400

    Wrap up load of analysis

--- analyze.py
+++ analyze.py
@@ -1,4 +1,24 @@
-def calcSignificance(week_1, week_2):
+from scipy.stats import mannwhitneyu
+import csv
+import numpy as np
+
+def loadCSV(filename_week_1, filename_week_2):
+    week_1 = []
+    week_2 = []
+    with open(filename_week_1, 'r') as f:
+        fieldnames = ['numHelped', 'crowdSize', 'sessionsCompleted', 'difficulty', 'comfort']
+        csv_reader = csv.DictReader(f, fieldnames=fieldnames)
+        for row in csv_reader:
+            week_1.append(np.array(row.values()))
+    with open(filename_week_2, 'r') as f:
+        fieldnames = ['numHelped', 'crowdSize', 'sessionsCompleted', 'difficulty', 'comfort']
+        csv_reader = csv.DictReader(f, fieldnames=fieldnames)
+        for row in csv_reader:
+            week_2.append(np.array(row.values()))
+    return week_1, week_2
+
+
+def calcMattWhitneyU(week_1, week_2):
     '''
     Run Mann Whitney U test on all 5 variables across week 1 and week 2. 
     week_1, week_2 is of the same format as the output of genData.
@@ -14,3 +34,15 @@ def calcSignificance(week_1, week_2):
     di_mwu = mannwhitneyu(w1_di, w2_di)
     comf_mwu = mannwhitneyu(w1_comf, w2_comf)
     return nh_mwu, cr_mwu, comp_mwu, di_mwu, comf_mwu
+
+
+directory = './sample_data/normal'
+
+week_1, week_2 = loadCSV('{0}/neg_correlation_week_1'.format(directory), '{0}/neg_correlation_week_2'.format(directory))
+print(calcMattWhitneyU(week_1, week_2))
+
+week_1, week_2 = loadCSV('{0}/no_correlation_week_1'.format(directory), '{0}/no_correlation_week_2'.format(directory))
+print(calcMattWhitneyU(week_1, week_2))
+
+week_1, week_2 = loadCSV('{0}/pos_correlation_week_1'.format(directory), '{0}/pos_correlation_week_2'.format(directory))
+print(calcMattWhitneyU(week_1, week_2))
