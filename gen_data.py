1607c834e99c5ebff3747e12d9f53ef78231e4d5
Pierre Theo Klein <pierre.klein@mail.mcgill.ca>
2019-04-02 15:17:40 -0400

    Function comments

--- gen_data.py
+++ gen_data.py
@@ -2,7 +2,16 @@ import random
 from scipy.stats import mannwhitneyu
 import numpy as np
 
+# Creates a new row of data
 def createRandEntry(nh, cr, comp, di, comf):
+    '''
+    Create a new row of data randomized according to normal distribution, for all 5 datasets.
+    nh: normal distribution parameters for number of students helped. Tuple: (mu, sigma)
+    cr: normal distribution parameters for crowd size. Tuple: (mu, sigma)
+    comp: normal distribution parameters for number of tutoring sessions completed. Tuple: (mu, sigma)
+    di: normal distribution parameters for difficulty of instruction. Tuple: (mu, sigma)
+    comf: normal distribution parameters for comfort level. Tuple: (mu, sigma)
+    '''
     numHelped = random.normalvariate(nh[0], nh[1])
     crowd = random.normalvariate(cr[0], cr[1])
     completion = random.normalvariate(comp[0], comp[1])
@@ -10,8 +19,17 @@ def createRandEntry(nh, cr, comp, di, comf):
     comfort = random.normalvariate(comf[0], comf[1])
     return np.array([numHelped, crowd, completion, difficulty, comfort])
 
-
+# Generate dataset for both methods
 def genData(mu1, sigma1, mu2, sigma2, num_data_pts):
+    '''
+    mu1: the mean of the first method.
+    sigma1: the sigma of the first method.
+    mu2: the mean of the second method.
+    sigma2: the sigma of the second method.
+    num_data_pts: the sample size of each week.
+
+    Returns a tuple-- the first item is the data for the first week, and the second item is the data for the second week.
+    '''
     week_1 = []
     week_2 = []
     week_1_params = [mu1, sigma1]
@@ -36,26 +54,51 @@ def genData(mu1, sigma1, mu2, sigma2, num_data_pts):
 
 
 def noCorrelation(num_data_pts):
+    '''
+    Generate a dataset with the same mean, variance for both weeks. They are both centered around 2.5, with a standard 
+    deviation of 0.833, so that 97% is at 5.
+    '''
     return genData(2.5, 0.833, 2.5, 0.833, num_data_pts)
 
 
 def positiveCorrelation(num_data_pts):
+    '''
+    Generate a dataset where the first week is centered at 2.5, and std. deviation of 0.833. 
+    Second week is centered at 4, with a std. deviation of 0.333.
+    '''
     return genData(2.5, 0.833, 4, 0.333, num_data_pts)
 
 
 def veryPositiveCorrelation(num_data_pts):
+    '''
+    Generate a dataset where the first week is centered at 1, and std. deviation of 0.333. 
+    Second week is centered at 4, with a std. deviation of 0.333.
+    '''
     return genData(1, 0.333, 4, 0.333, num_data_pts)
 
 
 def negativeCorrelation(num_data_pts):
+    '''
+    Generate a dataset where the first week is centered at 1, and std. deviation of 0.333. 
+    Second week is centered at 2.5, with a std. deviation of 0.833.
+    '''
     return genData(1, 0.333, 2.5, 0.833, num_data_pts)
 
 
 def veryNegativeCorrelation(num_data_pts):
+    '''
+    Generate a dataset where the first week is centered at 4, with a std. deviation of 0.333.
+    Second week is centered at 1, and std. deviation of 0.333. 
+    '''
     return genData(4, 0.333, 1, 0.333, num_data_pts)
 
 
 def calcSignificance(week_1, week_2):
+    '''
+    Run Mann Whitney U test on all 5 variables across week 1 and week 2. 
+    week_1, week_2 is of the same format as the output of genData.
+    Return the results for all 5 variables.
+    '''
     w1_nh, w1_cr, w1_comp, w1_di, w1_comf = [
         [row[i] for row in week_1] for i in range(len(week_1[0]))]
     w2_nh, w2_cr, w2_comp, w2_di, w2_comf = [
